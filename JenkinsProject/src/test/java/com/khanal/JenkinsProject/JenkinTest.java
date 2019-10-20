10c90cae667a65869a9f577e31a48196a1d80113
bunu <bkhanal55@gmail.com>
2019-10-20 10:50:48 -0500

    Initial commit.

---
+++ JenkinsProject/src/test/java/com/khanal/JenkinsProject/JenkinTest.java
@@ -0,0 +1,24 @@
+package com.khanal.JenkinsProject;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class JenkinTest {
+	@Test
+	public void someTest() {
+		assertTrue(true);
+		
+	}
+	@Test
+	public void some2Test() {
+		assertTrue(true);
+		
+	}
+	@Test
+	public void some3Test() {
+		assertTrue(true);
+		
+	}
+
+}
---
+++ JenkinsProject/src/test/java/com/khanal/JenkinsProject/JenkinTest2.java
@@ -0,0 +1,20 @@
+package com.khanal.JenkinsProject;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class JenkinTest2 {
+	@Test
+	public void someTest() {
+		assertTrue(true);
+		
+	}
+	@Test
+	public void some2Test() {
+		assertTrue(true);
+		
+	}
+	
+
+}
